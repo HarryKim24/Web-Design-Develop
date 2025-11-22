6d96600fcfaa77b0f6fa9a013ac85f49a55bf22f
HarryKim24 <tl9434@naver.com>
2025-11-22 16:24:12 +0900

    updated layoutM-5

--- js/layoutM-5.js
+++ js/layoutM-5.js
@@ -1,22 +1,15 @@
-const $mainmenus = document.querySelectorAll('.nav .mainmenu');
-const $header = document.querySelector('#header');
+const $menus = document.querySelectorAll('.nav > ul > li');
 
-$mainmenus.forEach(menu => {
+$menus.forEach(menu => {
   menu.addEventListener('mouseover', () => {
-    $header.classList.add('on');
-    const $submenus = document.querySelectorAll('.nav .mainmenu > li > .submenu');
-    $submenus.forEach(submenu => {
-      submenu.style.height = '200px';
-    })
+    const $submenu = menu.querySelector('.submenu');
+    $submenu.style.height = '180px';
   })
 })
 
-$mainmenus.forEach(menu => {
+$menus.forEach(menu => {
   menu.addEventListener('mouseout', () => {
-    $header.classList.remove('on');
-    const $submenus = document.querySelectorAll('.nav .mainmenu > li > .submenu');
-    $submenus.forEach(submenu => {
-      submenu.style.height = '0px';
-    })
+    const $submenu = menu.querySelector('.submenu');
+    $submenu.style.height = '0';
   })
 })
