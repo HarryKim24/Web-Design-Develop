e1fce8cb6ed5684897dbc147da661a6f9684d015
HarryKim24 <tl9434@naver.com>
2025-11-24 19:00:56 +0900

    add sliderS-4

---
+++ js/sliderS-4.js
@@ -0,0 +1,13 @@
+let currentIndex = 0;
+const slider = document.querySelectorAll('.slider');
+
+slider.forEach(el => el.style.opacity = '0')
+slider[0].style.opacity = '1';
+
+setInterval(() => {
+  let nextIndex = (currentIndex + 1) % slider.length;
+  slider[currentIndex].style.opacity = '0';
+  slider[nextIndex].style.opacity = '1';
+  slider.forEach(el => el.style.transition = '1s ease');
+  currentIndex = nextIndex;
+}, 1500);
